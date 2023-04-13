27ef65d40502e7e498205caec6b4e01e497af824
redaphid <iam@aaronherres.com>
2023-04-13 02:32:28 -0700

    get_playlist

--- scripts/dangerload.fish
+++ scripts/dangerload.fish
@@ -1,3 +1,16 @@
-function hello
-  echo "Hello, World!"
+#!/usr/bin/env fish
+function get_playlist --argument playlist
+  set -q playlist[1]; or set playlist $PLAYLIST
+  set -q playlist[1]; or set playlist "default"
+  echo $playlist
+end
+function select --argument playlist
+  set playlist (get_playlist $playlist)
+  echo $playlist
+end
+
+function run --argument playlist
+  set -q playlist[1]; or set playlist $PLAYLIST
+  set -q playlist[1]; or set playlist "default"
+  echo $playlist
 end
