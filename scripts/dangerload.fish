eefdac1a41f070eaca21b528a552c2fad9f3849b
redaphid <iam@aaronherres.com>
2023-04-13 03:25:42 -0700

    sort visualizer filenames; pick shortest

--- scripts/dangerload.fish
+++ scripts/dangerload.fish
@@ -13,17 +13,12 @@ function get_playlist_dir
   echo "$HOME/tools/projectM/playlists"
 end
 
-function select --argument playlist
-  set playlist (get_playlist $playlist)
-  echo $playlist
-end
-
 function run --argument directory
   set playlist (get_playlist $playlist)
   set playlist_dir (get_playlist_dir)
   set project_m_dir (get_project_m_dir)
   ls $playlist_dir/$playlist
-  $project_m_dir/projectMSDL --presetPath $playlist_dir/$playlist 2> $project_m_dir/projectM.log
+  $project_m_dir/projectMSDL --presetPath $playlist_dir/$playlist 2> $project_m_dir/projectM-$playlist.log
 end
 
 
@@ -32,19 +27,26 @@ function get_current_visualizer --argument playlist
   set -q playlist[1]; or set playlist "default"
 
   set visualizer_log (tail -n 1 $HOME/tools/projectM/projectM.log)
-  echo "Log:" $visualizer_log
   set visualizer_name (string split ":" $visualizer_log)[3]
   # echo character length before trim
-  echo "Length:" (string length $visualizer_name)
 
   # trim spaces
   set visualizer_name (string trim $visualizer_name)[1]
-  # echo character length after trim
-  echo "Length:" (string length $visualizer_name)
+
   # get playlist directory
   set playlist_dir (get_playlist_dir)
-  # search for a file that has a name that contains the visualizer name
-  echo "Searching for visualizer: '" $visualizer_name
-  set visualizer_file (find $playlist_dir/$playlist/ | grep -i $visualizer_name)
+
+  set visualizer_files (find $playlist_dir/$playlist/ | grep -i $visualizer_name )
+  # sort visualizer files by file name length
+  set smallest_length 9999
+  for visualizer_file in $visualizer_files
+    set visualizer_file_length (string length $visualizer_file)
+    if test $visualizer_file_length -lt $smallest_length
+      set smallest_length $visualizer_file_length
+      set visualizer_file $visualizer_file
+      echo "shortest visualizer: '" $visualizer_file "'"
+    end
+  end
+  set visualizer_file $visualizer_files[1]
   echo "Found visualizer: '" $visualizer_file "'"
 end
